55128f3f10ea0a66d1937fd8b43a09195fefffcf
LTPC106N\admin <d-yamaguchi@link-tech.jp>
2024-07-13 09:49:53 +0900

    モデルを切り替えられる機能の追加

---
+++ test/main-plus.py
@@ -0,0 +1,100 @@
+"""
+使用するモデルやパラメータの調整を可能にしたバージョン
+"""
+
+import chainlit as cl
+from chainlit.input_widget import Slider
+
+from llm_response import generate_message, SYSTEM_CONTENT
+
+# 会話の履歴を格納する変数
+message_history = [
+    {
+        "role":"system",
+        "content":SYSTEM_CONTENT
+    }
+]
+
+@cl.set_chat_profiles
+async def chat_profile():
+    """
+    画面の上部に表示されるモデル一覧を設定する
+    """
+    
+    return [
+        cl.ChatProfile(
+            name="gpt-35-turbo-16k",
+            markdown_description="The underlying LLM model is **gpt-35-turbo-16k**.",
+            icon="icon画像のURLを指定します。",
+        ),
+        cl.ChatProfile(
+            name="gpt-4o",  ## ★envに追い出すべきかも
+            markdown_description="The underlying LLM model is **gpt-4**.",
+            icon="icon画像のURLを指定します。",
+        ),
+    ]
+
+@cl.on_chat_start
+async def start():
+    """
+    Chatを開始したタイミングで一度だけ呼ばれる。
+    """
+    
+    # パラメータの設定項目を定義する
+    settings = await cl.ChatSettings(
+        [
+            Slider(
+                id="max_tokens",
+                label="最大応答",
+                initial=800,
+                min=1,
+                max=4000,
+                step=1
+            ),
+            Slider(
+                id="temperature",
+                label="温度パパラメータ",
+                initial=0,
+                min=0,
+                max=1,
+                step=0.01
+            )
+        ]
+    ).send()
+
+    await update_settings(settings)
+    
+@cl.on_settings_update
+async def update_settings(settings):
+    """
+    パラメータの設定を変更する。
+    """
+    cl.user_session.set("llm_parameters",settings)
+    
+@cl.on_message
+async def main(message: cl.Message):
+    """
+    ユーザーからメッセージが送られたら実行される関数
+    """
+    llm_parameter = cl.user_session.get("llm_parameters")
+    chat_profile = cl.user_session.get("chat_profile")
+    message_history.append({
+        "role":"user",
+        "content":message.content
+    })
+    
+    response = generate_message(
+        message_history,
+        model_name=chat_profile,
+        max_tokens=int(llm_parameter["max_tokens"]),
+        temperature=llm_parameter["temperature"]
+    )
+    
+    await cl.Message(
+        content=response["content"]
+    ).send()
+    
+    message_history.append({
+        "role":"assistant",
+        "content":response["content"]
+    })
