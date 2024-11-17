ef1b1fba3ed36d7c9bbe5d6331a393c699a83e85
LTPC106N\admin <d-yamaguchi@link-tech.jp>
2024-11-17 21:50:57 +0900

    https化における対応内容の取込

--- app/main.py
+++ app/main.py
@@ -8,7 +8,12 @@ from vector_search import vector_search, insert_log
 
 from chainlit.input_widget import TextInput
 
+from passlib.apache import HtpasswdFile
+
 load_dotenv()
+
+htpasswd_file_path = os.environ["CHAINLIT_AUTH_FILE"]
+ht = HtpasswdFile(htpasswd_file_path)
 
 # ファイル添付機能を非表示に
 cl.config.features.spontaneous_file_upload = None
@@ -24,10 +29,34 @@ message_history = [
     }
 ]
 
+def check_htpasswd(username, password, htpasswd_path):
+    try:
+#        ht = HtpasswdFile(htpasswd_path)
+        if ht.check_password(username, password):
+            return True
+        else:
+            return False
+    except FileNotFoundError:
+        print(f"File not found: {htpasswd_path}")
+        return False
+
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    htpasswd_file_path = os.environ["CHAINLIT_AUTH_FILE"]
+
+    if check_htpasswd(username, password, htpasswd_file_path):
+        return cl.User(
+            identifier=username, metadata={"role": username}
+        )
+    else:
+        return None
+
 def update_message_history(new_message):
     global message_history
     message_history.append(new_message)
-    
+
     # メッセージ数が最大数を超えた場合、古いメッセージを削除
     while len(message_history) > MAX_MESSAGES:
 
@@ -43,7 +72,7 @@ async def chat_profile():
     """
     画面の上部に表示されるモデル一覧を設定する
     """
-    
+
     return [
         cl.ChatProfile(
             name=os.environ["AZURE_GPT_4O_MINI_NAME"],
@@ -59,18 +88,18 @@ async def chat_profile():
             name="QA-Search",
             markdown_description="QA登録された情報から質問に近い、回答を探します。",
         ),
-        cl.ChatProfile(
-            name=os.environ["AZURE_GPT_4O_NAME"],
-            # markdown_description="The underlying LLM model is **gpt-4**.",
-            markdown_description="AzureOpenAIの**gpt-4**モデルを使用します。",
-            # icon="icon画像のURLを指定します。",
-        ),
-        cl.ChatProfile(
-            name=os.environ["AZURE_OPENAI_DEPLOY_NAME"],
-            # markdown_description="The underlying LLM model is **gpt-35-turbo-16k**.",
-            markdown_description="AzureOpenAIの**gpt-35-turbo-16k**モデルを使用します。\ngpt-4o-miniがコスト、性能としても上位互換のため、本モデルの使用は非推奨です。",
-            # icon="icon画像のURLを指定します。",
-        ),
+#        cl.ChatProfile(
+#            name=os.environ["AZURE_GPT_4O_NAME"],
+#            # markdown_description="The underlying LLM model is **gpt-4**.",
+#            markdown_description="AzureOpenAIの**gpt-4**モデルを使用します。",
+#            # icon="icon画像のURLを指定します。",
+#        ),
+#        cl.ChatProfile(
+#            name=os.environ["AZURE_OPENAI_DEPLOY_NAME"],
+#            # markdown_description="The underlying LLM model is **gpt-35-turbo-16k**.",
+#            markdown_description="AzureOpenAIの**gpt-35-turbo-16k**モデルを使用します。\ngpt-4o-miniがコスト、性能としても上位互換のため、本モデルの使用は非推奨です。",
+#            # icon="icon画像のURLを指定します。",
+#        ),
     ]
 
 @cl.on_chat_start
@@ -91,7 +120,7 @@ async def start():
                 label="カスタムプロンプト",
                 initial=SYSTEM_CONTENT,
                 # tooltip="ツールチップテスト",
-                description="システムプロンプト(AIの役割)を設定できます。https://docs.anthropic.com/ja/prompt-library/library などが参考になります。",
+                description="システムプロンプト(AIの役割)を設定できます。https://docs.anthropic.com/ja/prompt-library/library などが参考 になります。",
                 multiline=True
             ),
         ]
@@ -151,14 +180,14 @@ async def main(message: cl.Message):
     try:
         if chat_profile == "QA-Search":
             search_result = vector_search(message.content)
-            
+
             if search_result["user_message"]:
                 response_content = search_result["user_message"]
             else:
                 response_content = ""
                 for result in search_result["results"]:
                     response_content += f"{result['document']['content']}\n\n"
-            
+
             # VECTOR_SEARCHの場合のみログを保存
             insert_log(message.content, search_result)
         elif chat_profile == "Claude-3.5-Sonnet":
@@ -173,11 +202,11 @@ async def main(message: cl.Message):
     finally:
         # ローディングスピナーを非表示
         await loading_message.remove()
-    
+
     await cl.Message(content=response_content).send()
-    
+
     # アシスタントの応答を追加
     update_message_history({
         "role":"assistant",
         "content":response_content
-    })    
+    })
